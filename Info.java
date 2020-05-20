d9b2ea12441ffbe39388dcb4ae83d40ea74dfbf3
Denisssssssss <64546688+Denisssssssss@users.noreply.github.com>
2020-05-20 21:10:21 +0300

    info

---
+++ Info.java
@@ -0,0 +1,8 @@
+public class Info {
+
+    public static void main(String[] args) {
+
+        System.out.println("My name is Denis, I am a student from ITIS \n" +
+                "I'd like to attend web-programming classes");
+    }
+}
